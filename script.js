68d475df7a9fc75634a8d3b77dec5c40b75290a7
Vanessa <vanessambeattie@gmail.com>
2023-07-16 19:21:29 -0500

    Got Loop Working. Still need to implement retreat

--- script.js
+++ script.js
@@ -14,19 +14,24 @@ class Ship {
     }
 
     attack(Alien_in_Action){
-        console.log(`The USS is attacking....`);
+        console.log(`The USS is attacking....`) //${alienArmy[j]}`);
          // if random accuracy is <= alien.accuracy
+         //Aliens getting hit
          if (Math.random() < USS_Assembly1.accuracy) {
             console.log(`AlienShip has been hit!`);
-            //getting hit
             Alien_in_Action.hull -= this.firepower;
             console.log(`Alien's hull is ${Alien_in_Action.hull}`);
             if (Alien_in_Action.hull <= 0) {
                 console.log(`The AlienShip has been destroyed`);
-              //  break// get out of the look 
-         } else {
-            console.log(`Alien's escaped!`);
-            //check if target has been destroyed
+            //     if (alienArmy[j] != alienArmy[5]) {
+            //     //     click('Click if you want to retreat');
+            //     console.log('USS has retreated');
+            //         break // to get out of loop. USs retreated
+            //     }
+            //   //  break// get out of the look 
+            // } else {
+            // console.log(`Alien's escaped!`);
+            // //check if target has been destroyed
        
             
             // } else {
@@ -34,7 +39,7 @@ class Ship {
             
          }
         
-        }
+            }
     }
 
     attacked(USS_Assembly1) {
@@ -42,16 +47,20 @@ class Ship {
         // console.dir(this);
         // if random accuracy is <= alien.accuracy 
         if (Math.random() < alienArmy[j].accuracy) {//!alienarmy is not correct here. it needs to be alien_in_action
-            console.log(`USS has been hit!`);
             //getting hit
-            USS_Assembly1.hull -= this.firepower;
+            Math.floor(USS_Assembly1.hull -= this.firepower);
+            console.log(`USS has been hit!`);
             console.log(`USS's hull is ${USS_Assembly1.hull}`);
-            if (USS_Assembly1.hull <= 0) {
+            } else {
+                console.log('USS escaped attack!');
+            }
+             if (USS_Assembly1.hull <= 0) {
             console.log(`Game Over!!! The USS has been destroyed`);
             // break
-         } else {
-            console.log('The US scaped');
-         }
+            } 
+        //     else {
+        //         console.log('The US scaped');
+        //  }
             
             //check if target has been destroyed
             
@@ -61,10 +70,10 @@ class Ship {
             
         }
         
+    // }
+
+
     }
-
-
-}
 
     const USS_Assembly1 = new Ship('USS',20,5,7);
 
@@ -75,17 +84,24 @@ class Ship {
     super()
     this.shipName = Alien_Assembly;
     // this.hull = getRandomValue(3, 6);
-    // this.firepower = getRandomValue(2, 4);
-    // this.accuracy = getRandomValue(0.6, 0.8);
-    this.hull = Math.floor(Math.random() * 4) + 3;
-    this.firepower = Math.floor(Math.random() * 3) + 2;
-    this.accuracy = (Math.floor(Math.random() * 3) + 6) / 10;
-}
-}
+    this.firepower = getRandomValue(2, 4);
+    this.accuracy = getRandomValue(0.6, 0.8);
+    this.hull = Math.floor(Math.random() * 4 + 8);
+    // this.firepower = Math.random() * 2 + 2;
+    // this.accuracy = Math.random() * 0.6 + 0.6;
     
-    //creating Alien Army
-    const alienArmy = [];
-    for (let i = 0; i <= 5; i++) { 
+    //   Get random number within given range for Alien Ships key values
+    function getRandomValue(min, max) {
+    return Math.random() * (max - min +1) + min; // The maximum is inclusive and the minimum is inclusive
+    }                  
+//     const test = getRandomValue(3,6)
+//     console.log(test);
+}
+}
+
+//creating Alien Army
+const alienArmy = [];
+for (let i = 0; i <= 5; i++) { 
         // function create_new_ship() {
         // const alienArmy = new Alien_Assembly('Alien Army', this.hull, this.firepower, this. accuracy)
         alienArmy.push(new Alien_Assembly());
@@ -100,8 +116,25 @@ class Ship {
     //     if (USS_Assembly1.hull > 0) 
         {
         const Alien_in_Action = alienArmy[j];
+        // console.log(`The USS is attacking....${alienArmy[j]}`);
         USS_Assembly1.attack(alienArmy[j]);
+        if (alienArmy[j].hull > 0) {
+            alienArmy[j].attacked(USS_Assembly1)
+            if (USS_Assembly1.hull >= 0) {
+                USS_Assembly1.attack(alienArmy[j])
+             } else {
+                console.log('USS has been destroyed');
+             }
+        } else {
+                console.log('Alien Ship has been destoyed');}
+            j++
+
+        // }
+       
         Alien_in_Action.attacked(USS_Assembly1);
+        // if (alienArmy[j].hull <= 0) {
+
+        // }
         j++;
         if (alienArmy[5].hull <= 0) {
             console.log(`Game over!!! USS has won!`);     
@@ -111,13 +144,13 @@ class Ship {
         // } else {
         //     console.log('USS has won the battle');
         }
-    
-        }
+    }
+        
     //* randomnumber is giving negative numbers?
 
                  
                         
-
+    // console.log(`${alienArmy[j]}`)
 
 const Alien_Assembly1 = new Alien_Assembly('Alien',30,7,10)
 // event listener
@@ -201,9 +234,27 @@ attackBtn.addEventListener('click', USS_Assembly1.attack);
 // }
 // }                            
                         
-                        
-      //Get random number within given range for Alien Ships key values
-    // function getRandomValue(min, max) {
-    //  return Math.random() * (max - min +1) + min; // The maximum is inclusive and the minimum is inclusive
-    // }                  
- 
+// let j = 0;
+// while (USS_Assembly1.hull > 0 &&  j < 6)
+// // for (let j = 0; j <alienArmy.length; j++) {
+// //     if (USS_Assembly1.hull > 0) 
+//     {
+//     const Alien_in_Action = alienArmy[j];
+//     // console.log(`The USS is attacking....${alienArmy[j]}`);
+//     USS_Assembly1.attack(alienArmy[j]);
+   
+//     Alien_in_Action.attacked(USS_Assembly1);
+//     // if (alienArmy[j].hull <= 0) {
+
+//     // }
+//     j++;
+//     if (alienArmy[5].hull <= 0) {
+//         console.log(`Game over!!! USS has won!`);     
+//     } if ((USS_Assembly1.hull <= 0)) {
+//         console.log('USS ship has been destroyed!');
+    
+//     // } else {
+//     //     console.log('USS has won the battle');
+//     }
+
+//     }                   
